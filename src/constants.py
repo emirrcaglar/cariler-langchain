c2f484914401afff29077a6aa966e4132c69372a
emirrcaglar <emirrcaglar@gmail.com>
2025-07-04 11:29:00 +0300

    add constants

--- src/constants.py
+++ src/constants.py
@@ -1,5 +1,6 @@
 request_date = "data/api_req_date.json"
 DATA_FILE_PATH = "data/cari_hesap_hareketleri.csv"
 
+AI_MODEL = "o4-mini-2025-04-16"
 
 MAX_ROWS = 10 # max rows to send to agent if df too big (utils.check_shrink_df)
